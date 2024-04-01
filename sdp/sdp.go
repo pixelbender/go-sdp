a606fc47224cee94cb2bea4a9d45ac9b2432ee9e
Nikita Vorontsov <n.vorontsov@webinar.ru>
2024-04-01 19:01:47 +0300

    Add getPayloadName helper

--- sdp/sdp.go
+++ sdp/sdp.go
@@ -196,6 +196,13 @@ func (f *Format) String() string {
 	return f.Name
 }
 
+func (f *Format) GetName() string {
+	if f.Name == "" {
+		return GetPayloadName(f.Payload)
+	}
+	return f.Name
+}
+
 var epoch = time.Date(1900, time.January, 1, 0, 0, 0, 0, time.UTC)
 
 func isRTP(media, proto string) bool {
@@ -205,4 +212,39 @@ func isRTP(media, proto string) bool {
 	default:
 		return false
 	}
+}
+
+// https://datatracker.ietf.org/doc/html/rfc3551#section-6
+const numKnownTypes = 35
+
+var payloadTypeNames [numKnownTypes]string = [numKnownTypes]string{
+	0:  "PCMU",
+	3:  "GSM",
+	4:  "G723",
+	5:  "DVI4",
+	6:  "DVI4",
+	7:  "LPC",
+	8:  "PCMA",
+	9:  "G722",
+	10: "L16",
+	11: "L16",
+	12: "QCELP",
+	15: "G728",
+	16: "DVI4",
+	17: "DVI4",
+	18: "G729",
+	25: "CelB",
+	26: "JPEG",
+	28: "nv",
+	31: "H261",
+	32: "MPV",
+	33: "MP2T",
+	34: "H263",
+}
+
+func GetPayloadName(payload uint8) string {
+	if payload < numKnownTypes {
+		return payloadTypeNames[payload]
+	}
+	return ""
 }
