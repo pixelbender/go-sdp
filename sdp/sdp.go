a1571fd5457301c8040917a2671f966af78179ee
Nikita Vorontsov <vorontsov.nstu@yandex.ru>
2024-04-02 14:23:54 +0300

    fix sdp.NegotiateMode (#7)
    * fix sdp.NegotiateMode
    
    * Add getPayloadName helper
    
    * cr fixes
    
    ---------
    
    Co-authored-by: Nikita Vorontsov <n.vorontsov@webinar.ru>

--- sdp/sdp.go
+++ sdp/sdp.go
@@ -126,7 +126,7 @@ const (
 // NegotiateMode negotiates streaming mode.
 func NegotiateMode(local, remote string) string {
 	switch local {
-	case SendRecv:
+	case SendRecv, "":
 		switch remote {
 		case RecvOnly:
 			return SendOnly
@@ -137,12 +137,12 @@ func NegotiateMode(local, remote string) string {
 		}
 	case SendOnly:
 		switch remote {
-		case SendRecv, RecvOnly:
+		case SendRecv, "", RecvOnly:
 			return SendOnly
 		}
 	case RecvOnly:
 		switch remote {
-		case SendRecv, SendOnly:
+		case SendRecv, "", SendOnly:
 			return RecvOnly
 		}
 	}
@@ -189,6 +189,13 @@ func (f *Format) String() string {
 	return f.Name
 }
 
+func (f *Format) GetName() string {
+	if f.Name == "" {
+		return GetPayloadName(f.Payload)
+	}
+	return f.Name
+}
+
 var epoch = time.Date(1900, time.January, 1, 0, 0, 0, 0, time.UTC)
 
 func isRTP(media, proto string) bool {
@@ -198,4 +205,39 @@ func isRTP(media, proto string) bool {
 	default:
 		return false
 	}
+}
+
+// https://www.iana.org/assignments/rtp-parameters/rtp-parameters.xhtml
+var payloadTypeNames []string = []string{
+	0:  "PCMU",
+	3:  "GSM",
+	4:  "G723",
+	5:  "DVI4",
+	6:  "DVI4",
+	7:  "LPC",
+	8:  "PCMA",
+	9:  "G722",
+	10: "L16",
+	11: "L16",
+	12: "QCELP",
+	13: "CN",
+	14: "MPA",
+	15: "G728",
+	16: "DVI4",
+	17: "DVI4",
+	18: "G729",
+	25: "CelB",
+	26: "JPEG",
+	28: "nv",
+	31: "H261",
+	32: "MPV",
+	33: "MP2T",
+	34: "H263",
+}
+
+func GetPayloadName(payload uint8) string {
+	if int(payload) < len(payloadTypeNames) {
+		return payloadTypeNames[payload]
+	}
+	return ""
 }
